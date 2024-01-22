0eee3030727d03c7bf99ddc8ab7ec2f52bbd1bf1
rmvcorporations <98631567+rmvcorporations@users.noreply.github.com>
2024-01-23 01:33:29 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,35 @@
+const PORT = 8080;
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const API_KEY = "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";
+
+app.use(express.json());
+app.use(cors());
+
+app.listen(PORT, () => console.log("Your server is running on " + PORT));
+
+app.post("/", async (req, res) => {
+  const options = {
+    method: "POST",
+    headers: {
+      Authorization: `Bearer ${API_KEY}`,
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      model: "gpt-3.5-turbo",
+      messages: [{ role: "user", content: req.body.message }],
+      max_tokens: 100,
+    }),
+  };
+  try {
+    const response = await fetch(
+      "https://api.openai.com/v1/chat/completions",
+      options
+    );
+    const data = await response.json();
+    res.send(data);
+  } catch (error) {
+    console.log("Error " + error);
+  }
+});
